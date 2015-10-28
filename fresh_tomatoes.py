b8a580077d7a991f045f8f6d72ecb732df44cc7c
anirvan90 <anirvan.awatramani@gmail.com>
2015-10-29 01:11:04 +0530

    trying to fix trailer

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -5,8 +5,7 @@ import re
 
 # Styles and scripting for the page
 main_page_head = '''
-<!DOCTYPE html>
-<html lang="en">
+
 <head>
     <meta charset="utf-8">
     <title>Anirvans Movie Library!</title>
@@ -107,6 +106,8 @@ main_page_head = '''
 
 # The main page layout and title bar
 main_page_content = '''
+ <!DOCTYPE html>
+<html lang="en">
   <body>
     
     <!-- Trailer Video Modal -->
