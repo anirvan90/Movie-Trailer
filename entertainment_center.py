f13593dc304c63291742bdc103d4d0a980389019
anirvan90 <anirvan.awatramani@gmail.com>
2015-11-01 16:20:31 +0530

    Code Format
    - Format code in accordance with PEP8
    - Add comments to make code understandable by another person.

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,98 +1,187 @@
 import fresh_tomatoes
 import media
 
-
+# Create instances of Movie class by providing relevant data.
 kill_bill = media.Movie("Kill Bill",
-						"A former assassin, known simply as The Bride (Uma Thurman), wakes from a coma four years after her jealous ex-lover Bill (David Carradine) attempts to murder her on her wedding day.",
-						"https://upload.wikimedia.org/wikipedia/en/c/cf/Kill_bill_vol_one_ver.jpg",
-						"https://www.youtube.com/watch?v=7kSuas6mRpk",
-						"https://itunes.apple.com/us/movie/kill-bill-vol.-1/id432516627",
-						"http://www.rottentomatoes.com/m/kill_bill_vol_1/",
-						"000001")
-
+                        "A former assassin, known simply as The Bride\
+                        (Uma Thurman), wakes from a coma four years after her\
+                        jealous ex-lover Bill (David Carradine) attempts to\
+                        murder her on her wedding day.",
+                        "https://upload.wikimedia.org/wikipedia/en/c/cf/Kill_bill_vol_one_ver.jpg",   # noqa
+                        "https://www.youtube.com/watch?v=7kSuas6mRpk",
+                        "https://itunes.apple.com/us/movie/kill-bill-vol.-1/id432516627",   # noqa
+                        "http://www.rottentomatoes.com/m/kill_bill_vol_1/",
+                        "000001")
 
 the_departed = media.Movie("The Departed",
-							"South Boston cop Billy Costigan (Leonardo DiCaprio) goes under cover to infiltrate the organization of gangland chief Frank Costello (Jack Nicholson). As Billy gains the mobster's trust, a career criminal named Colin Sullivan (Matt Damon) infiltrates the police department and reports on its activities to his syndicate bosses. When both organizations learn they have a mole in their midst, Billy and Colin must figure out each other's identities to save their own lives.",
-							"https://upload.wikimedia.org/wikipedia/en/5/50/Departed234.jpg",
-							"https://www.youtube.com/watch?v=auYbpnEwBBg",
-							"https://itunes.apple.com/us/movie/the-departed/id284563157",
-							"http://www.rottentomatoes.com/m/departed/",
-							"000002")
+                           "South Boston cop Billy Costigan (Leonardo DiCaprio)\
+                           goes under cover to infiltrate the organization of\
+                           gangland chief Frank Costello (Jack Nicholson). As\
+                           Billy gains the mobster's trust, a career criminal\
+                           named Colin Sullivan (Matt Damon) infiltrates the\
+                           police department and reports on its activities to\
+                           his syndicate bosses. When both organizations learn\
+                           they have a mole intheir midst, Billy and Colin\
+                           must figure out each other's identities to save\
+                           their own lives.",
+                           "https://upload.wikimedia.org/wikipedia/en/5/50/Departed234.jpg",   # noqa
+                           "https://www.youtube.com/watch?v=auYbpnEwBBg",
+                           "https://itunes.apple.com/us/movie/the-departed/id284563157",   # noqa
+                           "http://www.rottentomatoes.com/m/departed/",
+                           "000002")
 
 
 inception = media.Movie("Inception",
-						"Dom Cobb (Leonardo DiCaprio) is a thief with the rare ability to enter people's dreams and steal their secrets from their subconscious. His skill has made him a hot commodity in the world of corporate espionage but has also cost him everything he loves. Cobb gets a chance at redemption when he is offered a seemingly impossible task: Plant an idea in someone's mind. If he succeeds, it will be the perfect crime, but a dangerous enemy anticipates Cobb's every move.",
-						"https://upload.wikimedia.org/wikipedia/en/7/7f/Inception_ver3.jpg",
-						"https://www.youtube.com/watch?v=8hP9D6kZseM",
-						"https://itunes.apple.com/us/movie/inception/id400763833",
-						"http://www.rottentomatoes.com/m/inception/",
-						"000003")
+                        "Dom Cobb (Leonardo DiCaprio) is a thief with the rare\
+                        ability to enter people's dreams and steal their\
+                        secrets from their subconscious. His skill has made\
+                        him a hot commodity in the world of corporate\
+                        espionage but has also cost him everything he loves.\
+                        Cobb gets a chance at redemption when he is offered\
+                        a seemingly impossible task: Plant an idea in\
+                        someone's mind. If he succeeds, it will be the perfect\
+                        crime, but a dangerous enemy anticipates Cobb's every\
+                        move.",
+                        "https://upload.wikimedia.org/wikipedia/en/7/7f/Inception_ver3.jpg",   # noqa
+                        "https://www.youtube.com/watch?v=8hP9D6kZseM",
+                        "https://itunes.apple.com/us/movie/inception/id400763833",   # noqa
+                        "http://www.rottentomatoes.com/m/inception/",
+                        "000003")
 
 
 the_party = media.Movie("The Party",
-						"While trying to tie his shoe, bumbling extra Hrundi V. Bakshi (Peter Sellers) unwittingly triggers explosives that destroy the set of an epic war film. The furious director tells executive Fred Clutterbuck (J. Edward McKinley) to fire him. Because of a misunderstanding, Bakshi instead mistakenly receives an invitation to an exclusive party at Clutterbuck's Hollywood mansion, where he proceeds to wreak havoc on partygoers as he stumbles through what will become the wildest night he's ever seen.",
-						"https://upload.wikimedia.org/wikipedia/en/4/4f/Party_moviep.jpg",
-						"https://www.youtube.com/watch?v=SU9s8L7Sewg",
-						"https://itunes.apple.com/us/movie/the-party/id834720314",
-						"https://www.rottentomatoes.com",
-						"000004")
+                        "While trying to tie his shoe, bumbling extra\
+                        Hrundi V. Bakshi (Peter Sellers) unwittingly triggers\
+                        explosives that destroy the set of an epic war film.\
+                        The furious director tells executive Fred Clutterbuck\
+                        (J. Edward McKinley) to fire him. Because of a\
+                        misunderstanding, Bakshi instead mistakenly receives\
+                        an invitation to an exclusive party at Clutterbuck's\
+                        Hollywood mansion, where he proceeds to wreak havoc\
+                        on partygoers as he stumbles through what will become\
+                        the wildest night he's ever seen.",
+                        "https://upload.wikimedia.org/wikipedia/en/4/4f/Party_moviep.jpg",   # noqa
+                        "https://www.youtube.com/watch?v=SU9s8L7Sewg",
+                        "https://itunes.apple.com/us/movie/the-party/id834720314",   # noqa
+                        "https://www.rottentomatoes.com",
+                        "000004")
 
 
-the_grand_budapest_hotel = media.Movie("The Grand Budapest Hotel",
-										"In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege. Gustave prides himself on providing first-class service to the hotel's guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave's lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.",
-										"https://upload.wikimedia.org/wikipedia/en/a/a6/The_Grand_Budapest_Hotel_Poster.jpg",
-										"https://www.youtube.com/watch?v=1Fg5iWmQjwk",
-										"https://itunes.apple.com/us/movie/the-grand-budapest-hotel/id828777452",
-										"http://www.rottentomatoes.com/m/the_grand_budapest_hotel/",
-										"000005")
+the_grand_budapest = media.Movie("The Grand Budapest Hotel",
+                                 "In the 1930s, the Grand Budapest Hotel is a\
+                                 popular European ski resort, presided over\
+                                 by concierge Gustave H. (Ralph Fiennes).\
+                                 Zero, a junior lobby boy, becomes Gustave's\
+                                 friend and protege. Gustave prides himself\
+                                 on provding first-class service to the\
+                                 hotel's guests, including satisfying the\
+                                 sexual\
+                                 needs of the many elderly women who stay\
+                                 there. When one of Gustave's lovers dies\
+                                 mysteriously,\
+                                 Gustave finds himself the recipient of a\
+                                 priceless painting and the chief suspect in\
+                                 her murder.",
+                                 "https://upload.wikimedia.org/wikipedia/en/a/a6/The_Grand_Budapest_Hotel_Poster.jpg",   # noqa
+                                 "https://www.youtube.com/watch?v=1Fg5iWmQjwk",
+                                 "https://itunes.apple.com/us/movie/the-grand-budapest-hotel/id828777452",   # noqa
+                                 "http://www.rottentomatoes.com/m/the_grand_budapest_hotel/",   # noqa
+                                 "000005")
 
 
 in_bruges = media.Movie("In Bruges",
-						"After a particularly difficult job, hit men Ray (Colin Farrell) and Ken (Brendan Gleeson) head to Belgium to hide out until things cool down. Ray hates the medieval city they land in, but Ken finds its beauty and peacefulness enchanting. Their experiences become increasingly surreal and possibly life-changing as they encounter tourists, locals, an American dwarf and a potential romance for Ray.",
-						"images/in-bruges.jpg",
-						"https://www.youtube.com/watch?v=p-gG2qo_l_A",
-						"https://itunes.apple.com/us/movie/in-bruges/id281077778",
-						"http://www.rottentomatoes.com/m/in_bruges/",
-						"000006")
+                        "After a particularly difficult job, hit men\
+                        Ray (Colin Farrell) and Ken (Brendan Gleeson)\
+                        head to Belgium to hide out until things cool\
+                        down. Ray hates the medieval city they land\
+                        in, but Ken finds its beauty and peacefulness\
+                        enchanting. Their experiences become increasingly\
+                        surreal and possibly life-changing as they\
+                        encounter tourists, locals, an American dwarf and\
+                        a potential romance for Ray.",
+                        "images/in-bruges.jpg",
+                        "https://www.youtube.com/watch?v=p-gG2qo_l_A",
+                        "https://itunes.apple.com/us/movie/in-bruges/id281077778",   # noqa
+                        "http://www.rottentomatoes.com/m/in_bruges/",   # noqa
+                        "000006")
 
 gone_girl = media.Movie("Gone Girl",
-						"In Carthage, Mo., former New York-based writer Nick Dunne (Ben Affleck) and his glamorous wife Amy (Rosamund Pike) present a portrait of a blissful marriage to the public. However, when Amy goes missing on the couple's fifth wedding anniversary, Nick becomes the prime suspect in her disappearance. The resulting police pressure and media frenzy cause the Dunnes' image of a happy union to crumble, leading to tantalizing questions about who Nick and Amy truly are.",
-						"https://upload.wikimedia.org/wikipedia/en/0/05/Gone_Girl_Poster.jpg",
-						"https://www.youtube.com/watch?v=esGn-xKFZdU",
-						"https://itunes.apple.com/us/movie/gone-girl/id917874539",
-						"http://www.rottentomatoes.com/m/gone_girl/",
-						"000007")
+                        "In Carthage, Mo., former New York-based writer\
+                        Nick Dunne (Ben Affleck) and his glamorous wife\
+                        Amy (Rosamund Pike) present a portrait of a\
+                        blissful marriage to the public. However, when\
+                        Amy goes missing on the couple's fifth wedding\
+                        anniversary, Nick becomes the prime suspect in\
+                        her disappearance. The resulting police pressure\
+                        and media frenzy cause the Dunnes' image of a\
+                        happy union to crumble, leading to tantalizing\
+                        questions about who Nick and Amy truly are.",
+                        "https://upload.wikimedia.org/wikipedia/en/0/05/Gone_Girl_Poster.jpg",   # noqa
+                        "https://www.youtube.com/watch?v=esGn-xKFZdU",
+                        "https://itunes.apple.com/us/movie/gone-girl/id917874539",   # noqa
+                        "http://www.rottentomatoes.com/m/gone_girl/",
+                        "000007")
 
 mystic_river = media.Movie("Mystic River",
-							"When the daughter (Emmy Rossum) of ex-con Jimmy Marcus (Sean Penn) is murdered, two of his childhood friends from the neighborhood are involved. Dave (Tim Robbins), a blue-collar worker, was the last person to see her alive, while Sean (Kevin Bacon), a homicide detective, is heading up the case. As Sean proceeds with his investigation, Jimmy conducts one of his own through neighborhood contacts. Eventually, Jimmy suspects Dave is the culprit and considers taking the law into his own hands.",
-							"https://upload.wikimedia.org/wikipedia/en/9/93/Mystic_River_poster.jpg",
-							"https://www.youtube.com/watch?v=AvQaTV_lBuk",
-							"https://itunes.apple.com/us/movie/mystic-river/id540717367",
-							"http://www.rottentomatoes.com/m/mystic_river/",
-							"000008")
+                           "When the daughter (Emmy Rossum) of ex-con\
+                           Jimmy Marcus (Sean Penn) is murdered, two\
+                           of his childhood friends from the neighborhood\
+                           are involved. Dave (Tim Robbins), a blue-collar\
+                           worker, was the last person to see her alive,\
+                           while Sean (Kevin Bacon), a homicide detective,\
+                           is heading up the case. As Sean proceeds with his\
+                           investigation, Jimmy conducts one of his own\
+                           through neighborhood contacts. Eventually, Jimmy\
+                           suspects Dave is the culprit and considers taking\
+                           the law into his own hands.",
+                           "https://upload.wikimedia.org/wikipedia/en/9/93/Mystic_River_poster.jpg",   # noqa
+                           "https://www.youtube.com/watch?v=AvQaTV_lBuk",
+                           "https://itunes.apple.com/us/movie/mystic-river/id540717367",   # noqa
+                           "http://www.rottentomatoes.com/m/mystic_river/",   # noqa
+                           "000008")
 
 american_hustle = media.Movie("American Hustle",
-								"Irving Rosenfeld (Christian Bale) dabbles in forgery and loan-sharking, but when he falls for fellow grifter Sydney Prosser (Amy Adams), things change in a big way. Caught red-handed by FBI agent Richie DiMaso (Bradley Cooper), Irv and Sydney are forced to work under cover as part of DiMaso's sting operation to nail a New Jersey mayor (Jeremy Renner). Meanwhile, Irv's jealous wife (Jennifer Lawrence) may be the one to bring everyone's world crashing down. Based on the 1970s Abscam case.",
-								"https://upload.wikimedia.org/wikipedia/en/8/85/American_Hustle_2013_poster.jpg",
-								"https://www.youtube.com/watch?v=ST7a1aK_lG0",
-								"https://itunes.apple.com/us/movie/american-hustle/id774456362",
-								"http://www.rottentomatoes.com/m/american_hustle/",
-								"000009")
+                              "Irving Rosenfeld (Christian Bale) dabbles in\
+                              forgery and loan-sharking, but when he\
+                              falls for\
+                              fellow grifter Sydney Prosser (Amy Adams),\
+                              things\
+                              change in a big way. Caught red-handed by FBI\
+                              agent Richie DiMaso (Bradley Cooper), Irv and\
+                              Sydney are forced to work under cover as part\
+                              of DiMaso's sting operation to nail a\
+                              New Jersey\
+                              mayor (Jeremy Renner). Meanwhile, Irv's jealous\
+                              wife (Jennifer Lawrence) may be the one\
+                              to bring\
+                              everyone's world crashing down. Based on the\
+                              1970s Abscam case.",
+                              "https://upload.wikimedia.org/wikipedia/en/8/85/American_Hustle_2013_poster.jpg",   # noqa
+                              "https://www.youtube.com/watch?v=ST7a1aK_lG0",
+                              "https://itunes.apple.com/us/movie/american-hustle/id774456362",   # noqa
+                              "http://www.rottentomatoes.com/m/american_hustle/",   # noqa
+                              "000009")
 
 cool_runnings = media.Movie("Cool Runnings",
-							"Four Jamaican bobsledders dream of competing in the Winter Olympics, despite never having seen snow. With the help of a disgraced former champion desperate to redeem himself, the Jamaicans set out to become worthy of Olympic selection, and go all out for glory.",
-							"https://upload.wikimedia.org/wikipedia/en/7/76/Coolrunnings.jpg",
-							"https://www.youtube.com/watch?v=wLlmymHRNZg",
-							"https://itunes.apple.com/us/movie/cool-runnings/id351314818",
-							"http://www.rottentomatoes.com/m/1046227-cool_runnings/",
-							"000010")
+                            "Four Jamaican bobsledders dream of competing\
+                            in the Winter Olympics, despite never having\
+                            seen snow. With the help of a disgraced former\
+                            champion desperate to redeem himself, the\
+                            Jamaicans set out to become worthy of Olympic\
+                            selection, and go all out for glory.",
+                            "https://upload.wikimedia.org/wikipedia/en/7/76/Coolrunnings.jpg",   # noqa
+                            "https://www.youtube.com/watch?v=wLlmymHRNZg",   # noqa
+                            "https://itunes.apple.com/us/movie/cool-runnings/id351314818",   # noqa
+                            "http://www.rottentomatoes.com/m/1046227-cool_runnings/",   # noqa
+                            "000010")
 
+# Append each movie to the movies list
+movies = [kill_bill, the_departed, inception, the_party,
+          the_grand_budapest, in_bruges, gone_girl,
+          mystic_river, american_hustle, cool_runnings]
 
-
-
-movies = [kill_bill, the_departed, inception, the_party, the_grand_budapest_hotel, in_bruges, gone_girl, mystic_river, american_hustle, cool_runnings]
-
+# Pass the movies list as a parameter to create the webpage
 fresh_tomatoes.open_movies_page(movies)
 
-#print (media.Movie.VALID_RATINGS)
+# print (media.Movie.VALID_RATINGS)
 print (media.Movie.__doc__)
